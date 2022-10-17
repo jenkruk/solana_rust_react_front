14b813fa5b438803e9003b07ee23a314813adfc9
Jennifer Kruk <jenkruk@gmail.com>
2022-10-17 11:32:11 -0700

    responsive improvements

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,29 +3,31 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className='footer'>
-			Using React to display current and new giphs on the Solana blockchain
-			through a Rust contract.
+			<p>
+				Using React to display current and new giphs on the Solana blockchain
+				through a Rust contract.
+			</p>
 			<div className='footer_links'>
 				<a
 					href='https://github.com/jenkruk/solana_rust_react_front'
 					target='_blank'
 					rel='noreferrer'
 				>
-					Github Repo for frontend
+					<span>🔗 </span> &nbsp; Github Repo for frontend
 				</a>
 				<a
 					href='https://github.com/jenkruk/solana_rust_backend'
 					target='_blank'
 					rel='noreferrer'
 				>
-					Github Repo for contract
+					<span>🔗 </span> &nbsp; Github Repo for contract
 				</a>
 				<a
 					href='https://explorer.solana.com/address/3qbP7m8xbYkYzm6avvS2RMo3DiLz2rvvZ1HjxunPS3XJ?cluster=devnet&utm_source=buildspace.so&utm_medium=buildspace_project'
 					target='_blank'
 					rel='noreferrer'
 				>
-					Contract on Solana
+					<span>🔗 </span> &nbsp; Contract on Solana
 				</a>
 			</div>
 		</div>
